bbfabf943bb33dffbbd7f01f25f623c01f3cdad9
naufalluth <naufaldvf123@gmail.com>
2025-01-02 14:31:33 +0700

    feat: enhance birthday webpage with audio and animation effects

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,7 @@ const animateElements = () => {
         skewX: "-15deg"
     }
 
+
     const tl = gsap.timeline();
 
     tl.to(".container", 0.1, {
@@ -34,6 +35,10 @@ const animateElements = () => {
             "+=2.5"
         )
         .from(".two", 0.7, { opacity: 0, y: -20, })
+        .from(".two p.text-one", 0.8, textFadeIn)
+        .to(".two p.text-one", 0.7, textFadeOut, "+=1.5")
+        .from(".two p.text-two", 0.7, textFadeIn)
+        .to(".two p.text-two", 0.7, textFadeOut, "+=1")
         .to(
             ".two",
             0.7,
@@ -41,9 +46,20 @@ const animateElements = () => {
                 opacity: 0,
                 y: 10
             },
-            "=+2.5"
+            "=+.8"
         )
         .from(".three", 0.7, { opacity: 0, y: -20, })
+        .from(".three p.wish-text1", 0.7, textFadeIn)
+        .to(".three p.wish-text1", 0.7, textFadeOut, "+=1")
+        .from(".three p.wish-text2", 0.7, textFadeIn)
+        .from(".three span.block-text", 0.7, textFadeIn)
+        .to(".three span.block-text", 1, {
+            color: "white", scale: 1.2, x: 10,
+            ease: "power2.inOut",
+        })
+        .to(".three p.wish-text2", 0.7, textFadeOut)
+        .from(".three p.wish-text3", 0.7, textFadeIn)
+        .to(".three p.wish-text3", 0.7, textFadeOut, "+=1")
         .to(
             ".three",
             0.7,
@@ -52,7 +68,7 @@ const animateElements = () => {
                 opacity: 0,
                 y: 10,
 
-            }, "+=2.5")
+            })
         .from(
             ".four",
             0.5,
@@ -63,6 +79,7 @@ const animateElements = () => {
             },
             "+=0.7"
         )
+
         .staggerFrom("div.four div.event1", 0.8, {
             opacity: 0,
             scale: 0.1,
@@ -73,7 +90,7 @@ const animateElements = () => {
             "div.four div.event1 h2.age span.bigNumber",
             0.8,
             {
-                scale: 3,
+                scale: 6,
                 opacity: 0,
                 rotation: 30,
                 ease: "expo.inOut"
@@ -123,10 +140,10 @@ const animateElements = () => {
             opacity: 0,
             y: 10,
         }, "+=2.0",)
-        .from(".nine", 0.7, textFadeIn)
-        .to(".nine", 0.7, textFadeOut, "+=2.0",)
+        .from(".nine", 0.7, textFadeIn, "+=2.0")
+        .to(".nine", 0.7, textFadeOut, "+=3",)
         .staggerFrom(".ten", 1, textFadeIn, 1.2)
-        .from(".flower img", 30, { opacity: 0 }, "+=2.0",)
+        .from(".flower img", 30, { opacity: 0 })
         .to(".flower img", 30, { opacity: 0 }, "+=2.0",)
 
 
@@ -139,4 +156,9 @@ const animateElements = () => {
 }
 
 
-document.addEventListener("DOMContentLoaded", animateElements); 
+document.addEventListener("DOMContentLoaded", function () {
+    const audio = document.querySelector("audio");
+    audio.play()
+    animateElements();
+
+}); 
